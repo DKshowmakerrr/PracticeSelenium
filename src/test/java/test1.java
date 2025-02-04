4957e894b796792654d99b7df4c871763a94d98e
DatNguyen <nguyendat5497@gmail.com>
2025-02-04 20:34:27 +0700

    new commit to git

---
+++ src/test/java/test1.java
@@ -0,0 +1,7 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test program.");
+        System.out.println("I'm adding a new line of code.");
+    }
+}
